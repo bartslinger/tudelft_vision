b0334fcd04a617ec077a3593c45b0923c63b99b7
Bart Slinger <bartslinger@gmail.com>
2018-12-22 19:08:49 +0100

    fix compilation issues

--- toolchains/bebop-toolchain.cmake
+++ toolchains/bebop-toolchain.cmake
@@ -3,11 +3,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # Specify the cross compiler
-SET(CMAKE_C_COMPILER   /opt/arm-2012.03/bin/arm-none-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /opt/arm-2012.03/bin/arm-none-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER   /opt/arm-2016.02-linaro/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-2016.02-linaro/bin/arm-linux-gnueabihf-g++)
 
 # Where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  /opt/arm-2012.03/arm-none-linux-gnueabi/libc)
+SET(CMAKE_FIND_ROOT_PATH  /opt/arm-2016.02-linaro/arm-linux-gnueabihf/libc)
 
 # Search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
